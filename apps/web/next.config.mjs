9a0fbc07b9230d355d405a14e471f7d1a6d3a024
josefanderberg <josef.anderberg@gmail.com>
2026-05-24 20:11:24 +0200

    fix: better cordinates maybe.

--- apps/web/next.config.mjs
+++ apps/web/next.config.mjs
@@ -1,4 +1,10 @@
 /** @type {import('next').NextConfig} */
+import { fileURLToPath } from 'url';
+import path from 'path';
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+
 const nextConfig = {
     reactStrictMode: false, // Sometimes helpful to disable for map/ref issues dev mode
     images: {
@@ -16,6 +22,8 @@ const nextConfig = {
     typescript: {
         ignoreBuildErrors: true,
     },
+    outputFileTracingRoot: path.join(__dirname, '../../'),
 };
 
 export default nextConfig;
+
